07e56f7c1c012d2d550d765a3bde77a6d2c5e677
Aircity <aircity@drrr.us>
2019-03-23 12:18:45 +0800

    feat:react only

--- index.js
+++ index.js
@@ -1,24 +1,38 @@
 module.exports = (api, projectOptions) => {
-  api.chainWebpack(webpackConfig => {
-    webpackConfig
+  api.chainWebpack(config => {
+    config
       .entry('app')
       .delete('./src/main.js')
       .add('./src/index.js')
       .end()
 
-    webpackConfig.module
+    config.module
       .rule('babel')
       .test(/\.js?$/)
       .use('babel-loader')
       .loader('babel-loader')
       .end()
 
-    webpackConfig.resolve.delete('vue-loader')
+    config.resolve.delete('vue-loader')
+    config.resolve.delete('vue-loader')
+    config.module.rules.delete('vue');
+    config.module.rule('css').oneOfs.delete('vue-modules');
+    config.module.rule('css').oneOfs.delete('vue');
+    config.module.rule('postcss').oneOfs.delete('vue-modules');
+    config.module.rule('postcss').oneOfs.delete('vue');
+    config.module.rule('scss').oneOfs.delete('vue-modules');
+    config.module.rule('scss').oneOfs.delete('vue');
+    config.module.rule('sass').oneOfs.delete('vue-modules');
+    config.module.rule('sass').oneOfs.delete('vue');
+    config.module.rule('less').oneOfs.delete('vue-modules');
+    config.module.rule('less').oneOfs.delete('vue');
+    config.module.rule('stylus').oneOfs.delete('vue-modules');
+    config.module.rule('stylus').oneOfs.delete('vue');
+    config.plugins.delete('vue-loader');
   })
 
   api.registerCommand(
-    'config',
-    {
+    'config', {
       description: 'test',
       usage: 'vue-cli-service config',
       options: {}
